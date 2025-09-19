174b65081cb664ec632bcb9a3b08cbbd020d4480
nextdev42 <jonathan.mrutu@protonmail.com>
2025-09-19 12:17:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -58,13 +58,24 @@ const parser = new Parser({
 
 async function fetchFeed(url) {
   try {
-    const res = await axios.get(url, { timeout: 20000 });
+    const res = await axios.get(url, {
+      timeout: 20000,
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                      "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive"
+      }
+    });
     return await parser.parseString(res.data);
   } catch (err) {
     console.error("Feed fetch error:", err.message, "| URL:", url);
     return { items: [], title: url };
   }
 }
+
 
 function extractImageFromItem(item) {
   let imgs = [];
