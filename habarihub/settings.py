d8833c1d9322f268ee84ccfa08f57b86dc3ba9da
nextdev42 <jonathan.mrutu@protonmail.com>
2025-09-19 10:09:39 +0300

    Update settings.py

--- habarihub/settings.py
+++ habarihub/settings.py
@@ -1,3 +1,12 @@
 ITEM_PIPELINES = {
     "habarihub.pipelines.HabarihubPipeline": 300,
 }
+# Enable downloader middlewares
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scrapy_user_agents.middlewares.RandomUserAgentMiddleware": 400,
+}
+
+# Optional: retry if blocked
+RETRY_ENABLED = True
+RETRY_TIMES = 3
